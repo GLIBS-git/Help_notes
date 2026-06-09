0504876eabf5bea84bc6fb9bf2838f7c12b51dab
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-06-10 01:49:22 +0300

    Update 01.03 Manage container.sh

--- 01. Docker/01.03 Manage container.sh
+++ 01. Docker/01.03 Manage container.sh
@@ -1,7 +1,9 @@
-# Run terminal in container
+# Run terminal session in container
 docker exec -it 057b96ea64eb /bin/bash
 docker exec -it 057b96ea64eb /bin/sh
 
+# Stop terminal session in container
+exit
 
 
 
