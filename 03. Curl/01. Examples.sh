0cbd613e6bb664591a95391a86327a5c0ac4cab4
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-06-17 02:47:39 +0300

    Update 01. Examples.sh

--- 03. Curl/01. Examples.sh
+++ 03. Curl/01. Examples.sh
@@ -2,7 +2,7 @@ curl -X POST "http://localhost:5050/api/command" -H "Content-Type: application/j
 curl -X POST "http://localhost:5050/api/command" -H "Content-Type: application/json" -d '{"command":"ping","parameter_1":"30"}'
 curl -X POST "http://localhost:5050/api/command" -H "Content-Type: application/json" -d '{"command":"exit"}'
 
-curl -X POST "http://localhost:5050/api/1.0/command" \
+curl -X POST "http://localhost:5050/api/v1/command" \
         -H "Content-Type: application/json" \
         -H "Accept: application/json" \
         -d @body.json
@@ -11,20 +11,23 @@ curl -X POST "http://localhost:5050/api/1.0/command" \
         -H @header.txt \
         -d @body.json
 
-curl -X GET "http://localhost:5050/api/1.0"
+curl -X GET "http://localhost:5050/api/v1"
 
-curl -v -X GET -G "http://localhost:5050/api/1.0/ping" \
+curl -v -X GET -G "http://localhost:5050/api/v1/ping" \
         --data-urlencode "a=Тест 0&Тест 1" \
         --data-urlencode "b=Тест 2" \
         --data-urlencode "c=Тест 3,Тест 4" \
         -H "Accept: application/json"
 
-curl -v -X GET -G "http://localhost:5050/api/1.0/ping/Тест!" \
+curl -v -X GET "http://localhost:5050/api/v1/ping/Тест!" \
         --url-query "a=Тест 0&Тест 1" \
         --url-query "b=Тест 2" \
         --url-query "c=Тест 3,Тест 4" \
         -H "Accept: application/json"
 
+# -v is better than -i
+curl -i --connect-timeout 5 "http://192.168.0.157:5050/api/v1"
+curl -v --connect-timeout 5 "http://192.168.0.157:5050/api/v1"
 
 
 
