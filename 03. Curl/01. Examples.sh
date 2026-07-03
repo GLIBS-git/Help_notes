b11b64b11889dca1a1d7fbd04c50362dcdaac82d
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-07-03 12:26:21 +0300

    Update 01. Examples.sh

--- 03. Curl/01. Examples.sh
+++ 03. Curl/01. Examples.sh
@@ -55,7 +55,7 @@ curl -v --connect-timeout 5 "http://localhost:5050/api/v1/test" ^
         --url-query "a=Тест 5&Тест 6" ^
         -H "Accept: application/json"
 
-curl -v -X POST --connect-timeout 5 "http://bp-aanikin-n:5252/run/replace" ^
+curl -v -o responce.json --connect-timeout 5 "http://bp-aanikin-n:5252/run/replace" ^
         -H "Content-Type: application/json" ^
         -H "Accept: application/json" ^
         -d @body.json
