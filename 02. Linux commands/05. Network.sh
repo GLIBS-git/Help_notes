a234b196897568dc0aebeac80a0157bbee019445
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-12 18:19:29 +0300

    Create 05. Network.sh

---
+++ 02. Linux commands/05. Network.sh
@@ -0,0 +1,8 @@
+# Send 4 commands like in Windows
+ping -c 4 ya.ru
+# Unlimited sending commands ([ Ctrl ] + [ C ] to stop)
+ping ya.ru
+# Ping over IPv4
+ping -4 ya.ru
+# Ping over IPv6
+ping -6 ya.ru
