fdda925d80d9c3b5e32dfd675e30c2ca03dcefca
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-14 01:22:53 +0300

    Update 01.01 Create-delete container.sh

--- 01. Docker/01.01 Create-delete container.sh
+++ 01. Docker/01.01 Create-delete container.sh
@@ -1,7 +1,7 @@
-# Test container
+# Create test container: (Puts output to cinsole; stops right after running) 
 docker run hello-world
 
-# Create container: 8080 - Docker host's port, 80 - container's port
+# Create container: (-p: 8080 - Docker host's port, 80 - container's port); (-d - runs detached)
 docker run -d -p 8080:80 nginx
 
 # Delete container
