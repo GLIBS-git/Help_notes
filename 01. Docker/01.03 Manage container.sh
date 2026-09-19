149d35a80a005bc8ccfd6cc9d2d6ee6a9f4c0aef
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-19 17:22:53 +0300

    Update container management commands with name support

--- 01. Docker/01.03 Manage container.sh
+++ 01. Docker/01.03 Manage container.sh
@@ -1,12 +1,12 @@
 # Run terminal session in container (container must be running)
-docker exec -it <container_id> /bin/bash
-docker exec -it <container_id> /bin/sh
+docker exec -it <container_id_or_name> /bin/bash
+docker exec -it <container_id_or_name> /bin/sh
 
 # Stop terminal session in container
 exit
 
 # Read past logs (works for stopped container)
-docker logs <container_id>
+docker logs <container_id_or_name>
 
 
 
