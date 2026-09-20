fc185e8f8053cb0024c09bfd86cb5405e77e4892
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-20 17:25:53 +0300

    Update 02. Files.sh

--- 02. Linux commands/02. Files.sh
+++ 02. Linux commands/02. Files.sh
@@ -4,7 +4,8 @@ cat file.txt
 # Output the file to the console by pages (space - next page, q - quit)
 less file.txt
 
-
+# Creates a file if not existing or updates file modification date-time
+touch Test-1.txt
 
 
 
