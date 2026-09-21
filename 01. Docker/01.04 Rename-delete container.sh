5000dbdb9cc4ceafafbaf2af3b90dde74d7e54ec
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-21 11:39:57 +0300

    Add Docker container rename and delete commands
    Added commands for renaming and deleting Docker containers.

--- 01. Docker/01.04 Rename-delete container.sh
+++ 01. Docker/01.04 Rename-delete container.sh
@@ -1,1 +1,29 @@
+# Rename container
+docker rename <old-name> <new-name>
+
+# Delete container
+docker rm <container_id_or_name>
+
+# Stop running container and delete
+docker rm -f <container_id_or_name>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
