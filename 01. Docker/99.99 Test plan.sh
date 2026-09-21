1cb7460697ffd6a1b1fa0fd131eaf713afbbe015
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-21 11:34:35 +0300

    Update 99.99 Test plan.sh

--- 01. Docker/99.99 Test plan.sh
+++ 01. Docker/99.99 Test plan.sh
@@ -1,9 +1,14 @@
 
 docker search java
 
+docker build -t my-java-app .
+docker run -it --rm --name my-running-app my-java-app
 
-
-
+FROM openjdk:11
+COPY . /usr/src/myapp
+WORKDIR /usr/src/myapp
+RUN javac Main.java
+CMD ["java", "Main"]
 
 
 
