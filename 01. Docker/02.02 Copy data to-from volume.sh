afefbecee7abc4e757f1946c120fff436c23cb8b
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-24 01:59:59 +0300

    Clarify comments in Docker copy script
    Updated comments for clarity on copying files from Docker volumes.

--- 01. Docker/02.02 Copy data to-from volume.sh
+++ 01. Docker/02.02 Copy data to-from volume.sh
@@ -1,8 +1,8 @@
-# Copy files from volume to host 
+# Copy files from volume to host. Both running or stopped containers.
 docker cp <container-id-or-name>:</path/in/container> </path/on/host>
 # Example: copy files from folder /app/data/ in container u4 to folder ~/Test/ on host
 docker cp u4:/app/data/. ~/Test/.
-# Example: copy the whole folder /app/data/ in container u4 to folder ~/Test/ on host
+# Example: copy the whole "data" folder from /app/data/ in container u4 to folder ~/Test/ (~/Test/data) on host
 docker cp u4:/app/data/ ~/Test/
 
 
