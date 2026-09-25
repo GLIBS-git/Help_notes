62e343ca5fb80a632e9a6be5139307f8ef880537
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-26 02:41:57 +0300

    Update 02.02 Copy data to-from volume.sh

--- 01. Docker/02.02 Copy data to-from volume.sh
+++ 01. Docker/02.02 Copy data to-from volume.sh
@@ -5,6 +5,12 @@ docker cp u4:/app/data/. ~/Test/.
 # Example: copy the whole "data" folder from /app/data/ in container u4 to folder ~/Test/ (~/Test/data) on host
 docker cp u4:/app/data/ ~/Test/
 
+# Copy files from host to volume. Both running or stopped containers.
+docker cp </path/on/host> <container-id-or-name>:</path/in/container>
+# Example: copy file 12.txt from host to folder /app/data/ in container u4
+docker cp ~/Test/12.txt u4:/app/data/
+
+
 
 
 
