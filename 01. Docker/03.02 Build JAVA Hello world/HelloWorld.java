242a41d00a279dede6b6be67e04700c55e7ec5a9
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-09-26 15:51:38 +0300

    Update HelloWorld.java to print custom messages

--- 01. Docker/03.02 Build JAVA Hello world/HelloWorld.java
+++ 01. Docker/03.02 Build JAVA Hello world/HelloWorld.java
@@ -1,3 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Docker container!");
+        System.out.println("This is my custom Docker image on JAVA");
+    }
+}
+
 
 
 
